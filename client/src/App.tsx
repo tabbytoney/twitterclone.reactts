5935458b6e8d7e494023115dd09653d73b6a60ba
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-07-02 18:40:47 -0500

    Install formik and yup, local token auth, login and signup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 import Users from './components/Users';
 import Home from './components/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any);
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  link: link as any,
   cache: new InMemoryCache(),
 });
 
@@ -15,8 +36,10 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <Routes>
-          <Route path='/' element={<Users />} />
-          <Route path='/home' element={<Home />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='/signup' element={<Signup />} />
         </Routes>
       </Router>
     </ApolloProvider>
